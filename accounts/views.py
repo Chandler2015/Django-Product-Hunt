567f19fa27df55946d41da21d27ceafa549f1a63
YuhangXiong <yxiong4@ualberta.ca>
2019-10-02 00:34:07 -0600

    add login and signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 def signup(request):
-    return render(request, signup.html)
+    if request.method == 'POST':
+        # the user wants to sign up
+        if request.POST['password'] == request.POST['confirmpassword']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {"error": 'User name has been taken.'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(
+                    request.POST['username'], password=request.POST['password'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {"error": 'Passwords do not match.'})
+
+    else:
+        return render(request, 'accounts/signup.html')
 
 
 def login(request):
-    return render(request, login.html)
+    return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return render(request, signup.html)
+    return render(request, 'accounts/signup.html')
