a881ffaf485425f7ce63d20d714949b74600fe6c
YuhangXiong <yxiong4@ualberta.ca>
2019-10-02 14:47:33 -0600

    add voting and show products on home page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 # make sure user is login to create page
 from django.contrib.auth.decorators import login_required
 from .models import Products
@@ -6,10 +6,11 @@ from django.utils import timezone
 
 
 def home(request):
-    return render(request, 'products/home.html')
+    products = Products.objects
+    return render(request, 'products/home.html', {'products': products})
 
 
-@login_required
+@login_required(login_url="/accounts/signup")
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
@@ -22,9 +23,23 @@ def create(request):
             product.pub_date = timezone.datetime.now()
             product.hunter = request.user
             product.save()
-            return redirect('home')
+            return redirect('/products/'+str(product.id))
         else:
             return render(request, 'products/create.html', {'error': 'All fields required.'})
 
     else:
         return render(request, 'products/create.html',)
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Products, pk=product_id)
+    return render(request, 'products/detail.html', {'product': product})
+
+
+@login_required(login_url="/accounts/signup")
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Products, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/'+str(product.id))
